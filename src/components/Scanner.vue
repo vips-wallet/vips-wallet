17915cb78202c61747bbf43711dd6d97520e6fd1
Koutarou Tanaka <from.kyushu.island@gmail.com>
2018-07-15 01:28:14 +0900

    add Scanner component

---
+++ src/components/Scanner.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-layout row wrap justify-center>
+    <qrcode-reader
+      @init="onCameraInit"
+      @decode="onCameraDecode"
+      :paused="paused"
+      :video-constraints="camera_config"
+    >
+      <v-btn
+        block
+        absolute
+        bottom
+        color="primary"
+        @click.stop="openFile"
+        v-t="'common.load_file'"
+      ></v-btn>
+    </qrcode-reader>
+  </v-layout>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import utils from '@/utils/utils'
+import CONST from '@/utils/const'
+
+export default {
+  name: 'QRScanner',
+  data () {
+    return {
+      paused: false,
+      camera_config: CONST.QRCODE_CAMERA_CONFIG
+    }
+  },
+  methods: {
+    onCameraInit (promise) {
+      promise.then(() => {
+        this.paused = false
+      }).catch(error => {
+        this.paused = true
+        this.$emit('initError', error)
+      })
+    },
+    onCameraDecode (content) {
+      this.paused = true
+      this.$globalEvent.$emit('camera-finished')
+      this.$emit('finish', content)
+    },
+    openFile () {
+      this.paused = true
+      utils.openQRCodeImage().then(code => {
+        this.$emit('finish', code.data)
+      }).catch(error => {
+        console.error(error)
+        this.$store.commit('setURI', null)
+        this.$emit('loadFileError', error)
+      }).finally(() => {
+        this.$globalEvent.$emit('camera-finished')
+      })
+    }
+  }
+}
+</script>
