95e3b0d3139d8b5544442f570abc5970b2cd8c25
Koutarou Tanaka <from.kyushu.island@gmail.com>
2018-08-17 13:31:22 +0900

    add software license in About.vue

--- src/components/wallet/settings/About.vue
+++ src/components/wallet/settings/About.vue
@@ -5,13 +5,25 @@
         <v-list-tile avatar>
           <v-list-tile-content>
             <v-list-tile-title v-t="'settings.about_detail.name'"></v-list-tile-title>
-            <v-list-tile-sub-title>vips-wallet</v-list-tile-sub-title>
+            <v-list-tile-sub-title>VIPS Wallet</v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile avatar>
           <v-list-tile-content>
             <v-list-tile-title v-t="'settings.about_detail.version'"></v-list-tile-title>
             <v-list-tile-sub-title>1.1.0-beta4</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-t="'settings.about_detail.license'"></v-list-tile-title>
+            <v-list-tile-sub-title><a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU General Public License (version 3)</a></v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-t="'settings.about_detail.sourcecode'"></v-list-tile-title>
+            <v-list-tile-sub-title><a href="https://github.com/vips-wallet/vips-wallet" target="_blank">GitHub</a></v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
